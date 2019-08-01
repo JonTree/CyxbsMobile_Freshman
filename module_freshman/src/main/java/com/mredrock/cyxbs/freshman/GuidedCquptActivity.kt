b79b025826ede2a3e102a75b1ab61973c6be0ed0
Tree <1246634075@qq.com>
2019-08-01 23:30:12 +0800

    增加7个activity

---
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/GuidedCquptActivity.kt
@@ -0,0 +1,12 @@
+package com.mredrock.cyxbs.freshman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuidedCquptActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.freshman_activity_guided_cqupt)
+    }
+}
--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/SayToYouActivity.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/SayToYouActivity.kt
@@ -3,10 +3,10 @@ package com.mredrock.cyxbs.freshman
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SayToYouActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.freshman_activity_say_to_you)
     }
 }
