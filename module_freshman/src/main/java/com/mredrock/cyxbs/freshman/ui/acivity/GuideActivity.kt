e71d95b4e8464b239d16b5b9331d73d26dd1b68e
Tree <1246634075@qq.com>
2019-08-02 23:20:35 +0800

    首页基本完成，校园指引正在添加Fragment当中

--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/ui/acivity/GuideActivity.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/ui/acivity/GuideActivity.kt
@@ -1,16 +1,15 @@
-package com.mredrock.cyxbs.freshman.ui
+package com.mredrock.cyxbs.freshman.ui.acivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mredrock.cyxbs.common.ui.BaseActivity
 import com.mredrock.cyxbs.freshman.R
 
-class OnlineCommunicationActivity : BaseActivity() {
+class GuideActivity : BaseActivity() {
     override val isFragmentActivity: Boolean = false
         //To change initializer of created properties use File | Settings | File Templates.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.freshman_activity_online_communication)
+        setContentView(R.layout.freshman_activity_guided)
     }
 }
