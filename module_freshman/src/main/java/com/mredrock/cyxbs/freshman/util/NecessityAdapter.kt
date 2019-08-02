e282339489c40f04b86573ca83852988514e487e
zixuan <931717954@qq.com>
2019-08-02 15:43:45 +0800

    bug调整

--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/util/NecessityAdapter.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/util/NecessityAdapter.kt
@@ -6,25 +6,25 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.mredrock.cyxbs.freshman.R
 
-//class NecessityAdapter : RecyclerView.Adapter<ViewHodler>() {
-//
-//
-//    override fun onCreateViewHolder(viewGroup: ViewGroup, p1: Int): ViewHodler {
-//
-////        return ViewHodler(LayoutInflater.from(viewGroup.context).inflate(R.layout,viewGroup,false))
-//    }
-//
-//    override fun onBindViewHolder(holder: ViewHodler, position: Int) {
-//
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return 9
-//    }
-//}
-//
-//class ViewHodler(item: View) : RecyclerView.ViewHolder(item){
-//    init {
-////        item.iv_background_picture
-//    }
-//}
+class NecessityAdapter constructor(): RecyclerView.Adapter<ViewHolder>() {
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, p1: Int): ViewHolder {
+
+        return ViewHolder(LayoutInflater.from(viewGroup.context).inflate(R.layout,viewGroup,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 9
+    }
+}
+
+class ViewHolder(item: View) : RecyclerView.ViewHolder(item){
+    init {
+//        item.iv_background_picture
+    }
+}
