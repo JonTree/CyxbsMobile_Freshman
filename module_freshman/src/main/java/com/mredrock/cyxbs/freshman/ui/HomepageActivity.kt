f01e46e8888adfc4867a69fb7842ecf7f9c815c5
zixuan <931717954@qq.com>
2019-08-02 10:45:23 +0800

    添加databingding

--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/ui/HomepageActivity.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/ui/HomepageActivity.kt
@@ -2,6 +2,7 @@ package com.mredrock.cyxbs.freshman.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.mredrock.cyxbs.freshman.R
 
 class HomepageActivity : AppCompatActivity() {
@@ -9,9 +10,11 @@ class HomepageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.freshman_activity_home)
-        val a =  Thread{
-         print("fdnvjsnfvjbfnjgb")
-        }
+
+        val a = DataBindingUtil.setContentView<com.mredrock.cyxbs.freshman.databinding.FreshmanActivityHomeBinding>(this,R.layout.freshman_activity_home)
+        a.addOnPropertyChangedCallback()
+        a.
+
     }
 
 
