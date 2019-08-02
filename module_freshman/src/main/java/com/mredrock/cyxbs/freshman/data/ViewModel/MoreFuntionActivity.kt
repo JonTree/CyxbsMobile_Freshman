8538a541768c741e83d98be74c55adb8107be668
zixuan <931717954@qq.com>
2019-08-02 14:07:14 +0800

    提交入学必备的xml文件

---
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/data/ViewModel/MoreFuntionActivity.kt
@@ -0,0 +1,13 @@
+package com.mredrock.cyxbs.freshman.data.ViewModel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mredrock.cyxbs.freshman.R
+
+class MoreFuntionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.freshman_activity_more_funtion)
+    }
+}
