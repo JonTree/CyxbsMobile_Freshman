2ba2e58661cfcf1fb0e57f5668b9a0f50b578737
zixuan <931717954@qq.com>
2019-08-02 23:24:18 +0800

    实验更改view与viewmodel之间的消息传递方式

--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/data/ViewModel/NecessityViewModel.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/data/ViewModel/NecessityViewModel.kt
@@ -1,6 +1,15 @@
 package com.mredrock.cyxbs.freshman.data.ViewModel
 
+import android.util.Log
 import com.mredrock.cyxbs.common.viewmodel.BaseViewModel
+import com.mredrock.cyxbs.freshman.data.bean.NecessityBean
+import com.mredrock.cyxbs.freshman.generated.callback.OnClickListener
+import org.greenrobot.eventbus.EventBus
 
 class NecessityViewModel :BaseViewModel() {
+    init {
+        Log.d("MyTag","viewmodel ready")
+        EventBus.getDefault().post(1)
+    }
+
 }
--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/ui/NecessityActivity.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/ui/NecessityActivity.kt
@@ -1,11 +1,16 @@
 package com.mredrock.cyxbs.freshman.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.mredrock.cyxbs.common.ui.BaseActivity
 import com.mredrock.cyxbs.freshman.R
 import com.mredrock.cyxbs.freshman.data.ViewModel.NecessityViewModel
+import com.mredrock.cyxbs.freshman.data.bean.NecessityBean
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 class NecessityActivity : BaseActivity() {
     override val isFragmentActivity: Boolean = false
@@ -17,10 +22,15 @@ class NecessityActivity : BaseActivity() {
         val titleBinding = DataBindingUtil.setContentView<com.mredrock.cyxbs.freshman.databinding.FreshmanRecycleItemNecessityTitleBinding>(this,R.layout.freshman_recycle_item_necessity_title)
         val itemBinding = DataBindingUtil.setContentView<com.mredrock.cyxbs.freshman.databinding.FreshmanRecycleItemNecessityItemBinding>(this,R.layout.freshman_recycle_item_necessity_item)
         val viewModel = ViewModelProviders.of(this).get(NecessityViewModel::class.java)
-        val vm : Deliverable = viewModel
+//        val vm : Deliverable = viewModel
 
 
         titleBinding.lifecycleOwner = this
         itemBinding.lifecycleOwner = this
+    }
+
+   @Subscribe
+    fun onEvent(a:Int){
+        Log.d("MyTag","event bus success")
     }
 }
