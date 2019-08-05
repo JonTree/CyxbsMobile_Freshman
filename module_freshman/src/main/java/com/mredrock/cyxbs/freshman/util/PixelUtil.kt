83d766c6e632dd3f923f23e2c1dea39dcb0a2411
zixuan <931717954@qq.com>
2019-08-05 23:27:21 +0800

    完成一个自定义view

--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/util/PixelUtil.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/util/PixelUtil.kt
@@ -14,4 +14,12 @@ object PixelUtil {
         val scale = context.resources.displayMetrics.density
         return (pxValue / scale + 0.5f).toInt()
     }
+    fun px2sp(context: Context, pxValue: Float): Int {
+        val fontScale = context.resources.displayMetrics.scaledDensity
+        return (pxValue / fontScale + 0.5f).toInt()
+    }
+    fun sp2px(context: Context, spValue: Float): Int {
+        val fontScale = context.resources.displayMetrics.scaledDensity
+        return (spValue * fontScale + 0.5f).toInt()
+    }
 }
