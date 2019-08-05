89fd929a1de8704ec604b5ad363fe894f69d3b26
Tree <1246634075@qq.com>
2019-08-05 18:51:34 +0800

    宿舍重构完成

--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/event/UpdataViewPagerAutoSlideEvent.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/event/UpdataViewPagerAutoSlideEvent.kt
@@ -1,6 +1,8 @@
 package com.mredrock.cyxbs.freshman.event
 
+import androidx.viewpager.widget.ViewPager
+
 /**
  * Created by Tree on 2019/8/4 21:16
  */
-class UpdataViewPagerAutoSlideEvent()
+data class UpdataViewPagerAutoSlideEvent(val viewPager: ViewPager)
--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/util/DormitoryViewPagerAdapter.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/util/DormitoryViewPagerAdapter.kt
@@ -3,12 +3,18 @@ package com.mredrock.cyxbs.freshman.util
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Interpolator
+import android.widget.Scroller
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.mredrock.cyxbs.freshman.R
 import com.mredrock.cyxbs.freshman.data.bean.CampusGuideBasicBean
 import com.mredrock.cyxbs.freshman.databinding.FreshmanViewPagerDormitoryPageBinding
+import com.mredrock.cyxbs.freshman.event.UpdataViewPagerAutoSlideEvent
+import kotlinx.android.synthetic.main.freshman_view_pager_dormitory_page.view.*
 import kotlinx.android.synthetic.main.freshman_view_pager_express_delivery_page.view.*
+import org.greenrobot.eventbus.EventBus
 
 /**
  * Created by Tree on 2019/8/3 22:37
@@ -20,6 +26,9 @@ class DormitoryViewPagerAdapter(val context: Context,val bean:CampusGuideBasicBe
 
     private val pagerList = ArrayList<View>()
 
+    private var isPause = false
+
+
 
 
     init {
@@ -28,9 +37,37 @@ class DormitoryViewPagerAdapter(val context: Context,val bean:CampusGuideBasicBe
                 context,
                 R.layout.freshman_view_pager_dormitory_page,
                 null
-            )
-            val bind = DataBindingUtil.bind<FreshmanViewPagerDormitoryPageBinding>(view)
+            ).apply {
+                vp_dormitory_carousel.adapter = DormitoryCarouselViewPagerAdapter(context as Context,bean)
+                vp_dormitory_carousel.currentItem = ((Int.MAX_VALUE/2) -1)
+                try {
+                    val aClass = ViewPager::class.java
+                    val sInterpolator = aClass.getDeclaredField("sInterpolator")
+                    sInterpolator.isAccessible = true
+                    val scroller = object : Scroller(context, sInterpolator.get(vp_dormitory_carousel) as Interpolator) {
+                        override fun startScroll(startX: Int, startY: Int, dx: Int, dy: Int, duration: Int) {
+                            //最后一个参数即viewpager自动滑动的时间
+                            super.startScroll(startX, startY, dx, dy, 2000)
+                        }
+                    }
+                    val mScroller = aClass.getDeclaredField("mScroller")
+                    mScroller.isAccessible = true
+                    mScroller.set(vp_dormitory_carousel, scroller)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
 
+                Thread{
+                    while (true) {
+                        Thread.sleep(3000)
+                        if (isPause) {
+                            continue
+                        }
+                        EventBus.getDefault().post(UpdataViewPagerAutoSlideEvent(vp_dormitory_carousel))
+                    }
+                }.start() }
+            val bind = DataBindingUtil.bind<FreshmanViewPagerDormitoryPageBinding>(view)
+            bind?.bean = msg
             pagerList.add(
                 view
             )}
