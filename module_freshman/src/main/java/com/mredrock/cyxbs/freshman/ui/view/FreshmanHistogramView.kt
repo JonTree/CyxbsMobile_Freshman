f15bdb9c056ef66be157991d9d5a885714fa7c6a
zixuan <931717954@qq.com>
2019-08-05 23:27:21 +0800

    完成一个自定义view

--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/ui/view/FreshmanHistogramView.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/ui/view/FreshmanHistogramView.kt
@@ -2,9 +2,7 @@ package com.mredrock.cyxbs.freshman.ui.view
 
 import android.content.Context
 import android.content.res.TypedArray
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import com.mredrock.cyxbs.common.utils.LogUtils
@@ -15,102 +13,149 @@ class FreshmanHistogramView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     private val coordinatePaint = Paint()
+    private val emptyPaint = Paint()
     private val framePaint = Paint()
     private val blueFillingPaint =Paint()
     private val pinkFillingPaint = Paint()
     private val lightBluePaint = Paint()
+    private val dataPaint = Paint()
+    private val list :List<Paint> = listOf(blueFillingPaint,pinkFillingPaint,lightBluePaint)
 
-    val viewWidth = PixelUtil.dp2px(context,282.toFloat())
-    val coordinateWidth =PixelUtil.dp2px(context,258.toFloat())
-    val viewHeight = PixelUtil.dp2px(context,194.toFloat())
-    val coordinateHeigh = PixelUtil.dp2px(context,175.toFloat())
-    val arrowConst = PixelUtil.dp2px(context,5.toFloat())
+    private var viewWidth :Float? = null
+    private var coordinateWidth :Float? = null
+
+    private var viewHeight :Float? = null
+    private var coordinateHeight :Float? = null 
+    private val arrowConst = PixelUtil.dp2px(context,5.toFloat())
+    private var mergin = 0.08
+    private var paintWidth = PixelUtil.dp2px(context, 1.toFloat())
+    private var textWidth = PixelUtil.sp2px(context,13.toFloat()).toFloat()
     init {
         setLayerType(LAYER_TYPE_SOFTWARE,null)
-        coordinatePaint.color = 0x5c7ffc
-        framePaint.color = 0x809bff
-        blueFillingPaint.color = 0x698aff
-        pinkFillingPaint.color = 0x516eff
-        lightBluePaint.color = 0x3cceff
+        coordinatePaint.color = Color.rgb(124,150,255)
+        dataPaint.color = Color.rgb(161,164,165)
+        framePaint.color = Color.rgb(104,130,255)
+        blueFillingPaint.color = Color.rgb(81,110,255)
+        pinkFillingPaint.color = Color.rgb(255,115,184)
+        lightBluePaint.color = Color.rgb(60,206,255)
 
-        var width = PixelUtil.dp2px(context, 1.toFloat())
-        coordinatePaint.strokeWidth = width.toFloat()
-        framePaint.strokeWidth = width.toFloat()
+
+        coordinatePaint.strokeWidth = paintWidth.toFloat()
+        framePaint.strokeWidth = paintWidth.toFloat()
 
         coordinatePaint.style = Paint.Style.FILL_AND_STROKE
         framePaint.style = Paint.Style.STROKE
         blueFillingPaint.style = Paint.Style.FILL
         pinkFillingPaint.style = Paint.Style.FILL
         lightBluePaint.style = Paint.Style.FILL
-    }
-    private var firstData:Int
-    private var secondData:Int
-    private var thirdData:Int
-    private var firstTitle:String?
-    private var secondTitle:String?
-    private var thirdTitle:String?
-    init {
-        val typedArray:TypedArray = context.obtainStyledAttributes(attrs, R.styleable.FreshmanHistogramView)
-        firstData = typedArray.getInt(R.styleable.FreshmanHistogramView_firstData,-1)
-        secondData = typedArray.getInt(R.styleable.FreshmanHistogramView_secondData,-1)
-        thirdData = typedArray.getInt(R.styleable.FreshmanHistogramView_thirdData,-1)
 
-        firstTitle = typedArray.getString(R.styleable.FreshmanHistogramView_firstTitle)
-        secondTitle = typedArray.getString(R.styleable.FreshmanHistogramView_secondTitle)
-        thirdTitle = typedArray.getString(R.styleable.FreshmanHistogramView_thirdTitle)
-        typedArray.recycle()
+        coordinatePaint.textAlign = Paint.Align.CENTER
+        dataPaint.textAlign = Paint.Align.CENTER
+        coordinatePaint.textSize = textWidth
+        dataPaint.textSize = textWidth
+        coordinatePaint.letterSpacing = 0.1.toFloat()
+        dataPaint.letterSpacing = 0.07.toFloat()
     }
+    var title:List<String>? = null
+    var data:List<Float>? = null
+
+    fun bindData(title:List<String>,data:List<Float>){
+        this.title = title
+        this.data = data
+        postInvalidate()
+    }
+
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
 
+        viewWidth = MeasureSpec.getSize(widthMeasureSpec).toFloat()
+        coordinateWidth = MeasureSpec.getSize(widthMeasureSpec).toFloat()*0.77.toFloat()
+        viewHeight = MeasureSpec.getSize(heightMeasureSpec).toFloat()
+        coordinateHeight = MeasureSpec.getSize(heightMeasureSpec).toFloat()*0.77.toFloat()
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 //        super.onMeasure(MeasureSpec.makeMeasureSpec(MeasureSpec.getMode(widthMeasureSpec),MeasureSpec.getSize(viewWidth)),
 //            MeasureSpec.makeMeasureSpec(MeasureSpec.getMode(heightMeasureSpec),MeasureSpec.getSize(viewHeight)))
+
+//        setMeasuredDimension(viewWidth,
+//           viewHeight)
+
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         LogUtils.d("MyTag","w="+w+"h="+h)
         super.onSizeChanged(w, h, oldw, oldh)
     }
-
+//    var titleMergin = 20.toFloat()
+//    fun titleMergin(mergin: Float){
+//        titleMergin = mergin
+//    }
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-//        canvas?.save()
-//        canvas?.translate(viewWidth.toFloat(),viewHeight.toFloat())
+        canvas?.save()
+//        var path :Path = Path()
+//        path.moveTo(titleMergin,0.toFloat())
+//        path.lineTo(0.toFloat(), (500).toFloat())
+//        canvas?.drawPath(path,coordinatePaint)
+//        canvas?.drawTextOnPath("挂科率前三",path,0.toFloat(),0.toFloat(),coordinatePaint)
 
-        canvas?.drawText("aaaaaaa",0.toFloat(),100.toFloat(),coordinatePaint)
+        canvas?.translate(((viewWidth!!-coordinateWidth!!)*0.5).toFloat(),((viewHeight!!-coordinateHeight!!)*0.5+coordinateHeight!!).toFloat())
+
+
         drawHorizontal(canvas)
         drawVertical(canvas)
-        drawRect(0,firstData,firstTitle,canvas,blueFillingPaint)
-        drawRect(1,secondData,secondTitle,canvas,pinkFillingPaint)
-        drawRect(2,thirdData,secondTitle,canvas,lightBluePaint)
+//        if(data != null && title != null){
+            repeat(3){
+                drawRect(it, data!![it], title!![it],canvas,list[it])
 
+            }
+//        }
     }
     private fun drawHorizontal(canvas: Canvas?){
-        canvas?.drawLine(0.toFloat(),0.toFloat(),coordinateWidth.toFloat(),0.toFloat(),coordinatePaint)
-        canvas?.drawLine(coordinateWidth.toFloat()-PixelUtil.dp2px(context,4.toFloat()),
-            PixelUtil.dp2px(context,3.toFloat()).toFloat(),coordinateWidth.toFloat(),0.toFloat(),coordinatePaint)
-        canvas?.drawLine(coordinateWidth.toFloat()-PixelUtil.dp2px(context,4.toFloat()),
-            -PixelUtil.dp2px(context,3.toFloat()).toFloat(),coordinateWidth.toFloat(),0.toFloat(),coordinatePaint)
+        canvas?.drawLine(0.toFloat(),0.toFloat(),coordinateWidth!!.toFloat(),0.toFloat(),coordinatePaint)
+        canvas?.drawLine(coordinateWidth!!.toFloat()-PixelUtil.dp2px(context,4.toFloat()),
+            PixelUtil.dp2px(context,3.toFloat()).toFloat(),coordinateWidth!!.toFloat(),0.toFloat(),coordinatePaint)
+        canvas?.drawLine(coordinateWidth!!.toFloat()-PixelUtil.dp2px(context,4.toFloat()),
+            -PixelUtil.dp2px(context,3.toFloat()).toFloat(),coordinateWidth!!.toFloat(),0.toFloat(),coordinatePaint)
 
     }
     private fun drawVertical(canvas: Canvas?){
-        canvas?.drawLine(0.toFloat(),0.toFloat(),0.toFloat(),coordinateHeigh.toFloat(),coordinatePaint)
-        canvas?.drawLine(0.toFloat(),coordinateHeigh.toFloat(),PixelUtil.dp2px(context,3.toFloat()).toFloat(),
-            coordinateHeigh.toFloat()-PixelUtil.dp2px(context,4.toFloat()).toFloat(),coordinatePaint)
-        canvas?.drawLine(0.toFloat(),coordinateHeigh.toFloat(),-PixelUtil.dp2px(context,3.toFloat()).toFloat(),
-            coordinateHeigh.toFloat()-PixelUtil.dp2px(context,4.toFloat()).toFloat(),coordinatePaint)
+        canvas?.drawLine(0.toFloat(),0.toFloat(),0.toFloat(),-coordinateHeight!!.toFloat(),coordinatePaint)
+        canvas?.drawLine(0.toFloat(),-coordinateHeight!!.toFloat(),PixelUtil.dp2px(context,3.toFloat()).toFloat(),
+            -coordinateHeight!!.toFloat()+PixelUtil.dp2px(context,4.toFloat()).toFloat(),coordinatePaint)
+        canvas?.drawLine(0.toFloat(),-coordinateHeight!!.toFloat(),-PixelUtil.dp2px(context,3.toFloat()).toFloat(),
+            -coordinateHeight!!.toFloat()+PixelUtil.dp2px(context,4.toFloat()).toFloat(),coordinatePaint)
     }
-    private fun drawRect(index:Int,data:Int,title:String?,canvas: Canvas?,insidePaint:Paint){
+    private fun drawRect(index:Int,data:Float,title:String?,canvas: Canvas?,insidePaint:Paint){
         var i =index*2+1
-        var rect:Rect = Rect(PixelUtil.dp2px(context ,(i*35).toFloat()),data/100*(PixelUtil.dp2px(context,coordinateHeigh.toFloat())),
-            PixelUtil.dp2px(context ,((i+1)*35).toFloat()),PixelUtil.dp2px(context ,1.toFloat()))
+
+
+        var pix = (coordinateWidth!!/11.5*(index*3.5+1)).toFloat()
+
+
+        LogUtils.d("MyTag","after:"+((data/100)*(coordinateHeight!!.toFloat())).toInt()+"before:"+coordinateHeight!!.toFloat())
+        var rect:Rect = Rect(pix.toInt(),-((data/100)*coordinateHeight!!.toFloat()).toInt(),
+            (pix+0.13*coordinateWidth!!).toInt(),-PixelUtil.dp2px(context ,1.toFloat()))
 
         canvas?.drawRect(rect,framePaint)
         canvas?.drawRect(rect,insidePaint)
 
-        coordinatePaint.textAlign = Paint.Align.CENTER
-        if(title!= null)
-            canvas?.drawText(title,PixelUtil.dp2px(context ,((i+0.5)*35).toFloat()).toFloat(),-PixelUtil.dp2px(context ,(16).toFloat()).toFloat(),coordinatePaint)
+
+        if(title!= null) {
+            canvas?.drawText(
+                title,
+                (pix+0.07*coordinateWidth!!).toFloat(),
+                (mergin*coordinateHeight!!).toFloat(),
+                coordinatePaint
+            )
+            canvas?.drawText("$data%",(pix+0.07*coordinateWidth!!).toFloat(),
+                -((data/100)*coordinateHeight!!.toFloat()).toInt()-PixelUtil.dp2px(context, (12).toFloat()).toFloat(),dataPaint)
+        }
+    }
+
+    fun setTextMargin(mergin :Double){
+        this.mergin = mergin
+    }
+    fun setTextSize(size:Float){
+        textWidth = size
     }
 }
