0db611e4dae410d82645418b8db7f184d39e501b
zixuan <931717954@qq.com>
2019-08-09 17:15:58 +0800

    再次完成部分viewmodel

--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/event/GuideDataEvent.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/event/GuideDataEvent.kt
@@ -6,7 +6,5 @@ import com.mredrock.cyxbs.freshman.data.bean.GuideBusBean
 /**
  * Created by Tree on 2019/8/9 0:16
  */
-class GuideDataEvent{
-    lateinit var  guideBusBean: GuideBusBean
-    lateinit var campusSightseeingBean: CampusSightseeingBean
+class GuideDataEvent (val busBean: GuideBusBean,val sightseeingBean: CampusSightseeingBean){
 }
