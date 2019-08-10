05eaaf17bbd9dfbd504869bbb467a12f9f3f94fc
zixuan <931717954@qq.com>
2019-08-10 16:20:24 +0800

    增加半个自定义view 改动信封的透明度

--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/ui/view/successView.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/ui/view/successView.kt
@@ -4,17 +4,40 @@ import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
 import android.view.View
+import com.mredrock.cyxbs.freshman.util.PixelUtil
 
 class successView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    private var viewWidth :Int? = null
+    private var viewHeight :Int? = null
+    private var degree :Float? =null
 
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        viewWidth = if(MeasureSpec.getMode(widthMeasureSpec) == MeasureSpec.EXACTLY){
+            MeasureSpec.getSize(widthMeasureSpec)
+        } else{
+            PixelUtil.dp2px(context,26.5f)
+        }
+        viewHeight = if(MeasureSpec.getMode(heightMeasureSpec) == MeasureSpec.EXACTLY){
+            MeasureSpec.getSize(widthMeasureSpec)
+        }else{
+            PixelUtil.dp2px(context,26.5f)
+        }
 
+        setMeasuredDimension(viewWidth!!,viewHeight!!)
     }
 
     override fun onDraw(canvas: Canvas?) {
-        super.onDraw(canvas)
+
+    }
+    private fun drawCircle(canvas: Canvas?){
+        canvas?.save()
+
+        canvas?.rotate(180f)
+        if(degree != null){
+
+        }
     }
 }
